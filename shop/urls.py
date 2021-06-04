a21a9d6bc86d65a016e10f91b54b4a3ed69b85c7
tg21 <gautamtushar21@gmail.com>
2021-06-04 21:57:49 +0530

    added view for shop login

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.firstFun,name='firstFun')
+    path('',views.firstFun,name='firstFun'),
+    path('login',views.login,name='login'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+
+from .models import user
+import json
 # Create your views here.
 
 
 def firstFun(request):
     return HttpResponse("You Either Die a hero, or you live long enough to see yourself become a villian")
+
+def login(request):
+    if(request.method == "POST"):
+        try:
+            received_json_data = json.loads(request.body.decode("utf-8"))
+            u = user.Objects.get(first_name=received_json_data.emailId)
+            if(u != None):
+                return JsonResponse({'data':'authorized'})
+            return JsonResponse({'data':'unauthorized'})
+        except Exception as e:
+            return HttpResponse(str(type(e)))
+    else:
+        return HttpResponse("You Don't GET it. Get it?")
