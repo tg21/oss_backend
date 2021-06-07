c6a59093a052b788eedf533644eed1e9d00838e3
tg21 <gautamtushar21@gmail.com>
2021-06-07 21:30:13 +0530

    added access to all db tables

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import user,item
+from .models import user,item,category,cart,order,inventory
 # Register your models here.
 
 admin.site.register(user)
 admin.site.register(item)
+admin.site.register(category)
+admin.site.register(cart)
+admin.site.register(order)
+admin.site.register(inventory)
